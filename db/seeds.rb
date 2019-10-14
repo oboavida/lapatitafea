cfbd72b5375adf596963e1ef219e197b8ad32153
Antonio Boavida <antonioruela@gmail.com>
2019-10-15 00:20:57 +0200

    New seeds models and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all seeds.."
+
+Collage.destroy_all
+
+puts "Done!... Creating new seeds..."
+
+Collage.create(
+  nombre: '¡Cómo no!',
+  medidas: "30 x 15 cm",
+  papel: "Fedrigoni extra fino",
+  tecnica: "Collage analógico",
+  precio: 50,
+  disponibilidad: true,
+  imagen: "https://i.imgur.com/T0mMYd4.jpg"
+  )
+
+
+Collage.create(
+  nombre: 'El nacimiento del Swing',
+  medidas: "30 x 15 cm",
+  papel: "Fedrigoni extra gordo",
+  tecnica: "Collage analógico",
+  precio: 55,
+  disponibilidad: true,
+  imagen: "https://i.imgur.com/T0mMYd4.jpg"
+  )
+
+
+Collage.create(
+  nombre: 'Lluvia en verano',
+  medidas: "30 x 15 cm",
+  papel: "Fedrigoni fino",
+  tecnica: "Collage analógico",
+  precio: 157,
+  disponibilidad: false,
+  imagen: "https://i.imgur.com/T0mMYd4.jpg"
+  )
+
+puts "All done!"
