fa3cc114063c7e89830112cf169ca4f02adfe664
Antonio Boavida <antonioruela@gmail.com>
2019-10-27 01:10:04 +0200

    Added Admin and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'collages#index'
-  get '/collages/:id', to: 'collages#show', as: 'collages'
+  # get '/collages/:id', to: 'collages#show', as: 'collages'
+  # get '/collages/new', to: 'collages#new', as: 'collages_new'
+  resources :collages
 end
