b00790c02958544614f4401cb41b3f9721773acc
Antonio Boavida <antonioruela@gmail.com>
2019-10-27 01:40:13 +0200

    authorization

--- app/controllers/collages_controller.rb
+++ app/controllers/collages_controller.rb
@@ -1,5 +1,5 @@
 class CollagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @collages = Collage.all
